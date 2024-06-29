f0aebbf16fa7fb01a497440b288940be5ccad28b
Hama <pashazad@msu.edu>
2024-06-29 13:51:30 -0400

    Add function to create grid in script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const container = document.querySelector('.container');
+const gridWidth = 16;
+const gridHeight = 16;
+
+function createGrid(width, height) {
+  for (let i = 0; i < width * height; i++) {
+    const div = document.createElement('div');
+    div.classList.add('grid-item');
+    container.appendChild(div);
+  }
+}
