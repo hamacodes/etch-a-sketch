4ed0d342ca25f48a18da0b2f1c491835cc108e79
Hama <pashazad@msu.edu>
2024-06-30 18:44:01 -0400

    Refactor script.js to create grid using provided container

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-const container = document.querySelector('.container');
-const gridWidth = 16;
-const gridHeight = 16;
-
-function createGrid(width, height) {
+function createGrid(container, width, height) {
   for (let i = 0; i < width * height; i++) {
     const div = document.createElement('div');
     div.classList.add('grid-item');
     container.appendChild(div);
+    console.log('div created');
   }
 }
 
-createGrid(gridWidth, gridHeight);
+
+// Website script
+const container = document.querySelector('.container');
+const gridWidth = 16;
+const gridHeight = 16;
+
+createGrid(container, gridWidth, gridHeight);
